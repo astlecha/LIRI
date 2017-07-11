abca418b8af3a2c5e593ba00634acb98b0091384
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-07-11 09:11:50 -0700

    adds switch method

--- liri.js
+++ liri.js
@@ -1,40 +1,75 @@
 var fs = require("fs");
+var action = process.argv[2];
 var keys = "";
 
-fs.readFile("keys.js", "utf8", function(error, data){
-	if(error){
-		console.log(error);
-	}
-	else{
-		keys = data;
-	}
-})
-
-
-//Request npm package after it has been installed
-var request = require("request");
-//Store arguments in an array
-var nodeArgs = process.argv
-//Store movie name in empty var
-var movieName = "";
-
-//Loop through the words in the node argument to define global var movieName
-for(var i = 2; i < nodeArgs.length; i++){
-	if (i > 2 && i < nodeArgs.length){
-		//Add plus signs between multiple words 
-		movieName = movieName + "+" + nodeArgs[i]
-	}
-	else{
-		movieName += nodeArgs[i];
-	}
+switch(action){
+	case "my-tweets":
+		tweets();
+		break;
+	case "spotify-this-song":
+		break;
+	case "movie-this":
+		movie();
+		break;
+	case "do-what-it-says":
+		random();
+		break;
 }
 
-//Define the OMDB Query URL
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+//my-tweets command
+function tweets(){
+	fs.readFile("keys.js", "utf8", function(error, data){
+		if(error){
+			console.log(error);
+		}
+		else{
+			keys = data;
+		}
+	})
+}
 
-request(queryUrl, function(error, response, body) {
-	//If there's no error and the response code is 200, log the movie info
-	if (!error && response.statusCode===200){
-		console.log(JSON.parse(body));
+//spotify-this-song command
+
+//movie-this command
+function movie() {
+	//Request npm package after it has been installed
+	var request = require("request");
+	//Store arguments in an array
+	var nodeArgs = process.argv
+	//Store movie name in empty var
+	var movieName = "";
+
+	//Loop through the words in the node argument to define global var movieName
+	for(var i = 2; i < nodeArgs.length; i++){
+		if (i > 2 && i < nodeArgs.length){
+			//Add plus signs between multiple words 
+			movieName = movieName + "+" + nodeArgs[i]
+		}
+		else{
+			movieName += nodeArgs[i];
+		}
 	}
-})
+
+	//Define the OMDB Query URL
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+	request(queryUrl, function(error, response, body) {
+		//If there's no error and the response code is 200, log the movie info
+		if (!error && response.statusCode===200){
+			console.log(JSON.parse(body));
+		}
+	})
+}
+
+//do-what-it-says command (aka random.txt)
+function random(){
+	fs.readFile("random.txt", "utf8", function(error, data){
+		if(error){
+			console.log(error);
+		}
+		else{
+			console.log(data);
+			return data;
+		}
+	})
+}
