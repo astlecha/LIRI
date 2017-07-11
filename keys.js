827bb3a0c24c4510c0c2a94bd965aa30fc6fece7
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-07-10 20:21:14 -0700

    adds twitter key info

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '5CdYfwF59SlG2zzV4HX8DX1Pb',
+  consumer_secret: 'Qaq9GGMvnO5m3Z0i98Qr6RG4UxClNEjLdME2EJNM2h5xRaPbgC',
+  access_token_key: '505025396-0yNVPF2AdAf09LrYkDgypCzHllfNl0JFAUp8CAGn',
+  access_token_secret: 'fmFi3BJKmFLwjXaY727efEB1I1c7th1Az6kbNnkw23Qjx',
 }
